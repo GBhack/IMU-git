c2525518b5bc15c2a384c3224201e85d60aca791
GBhack <gbmail01@gmail.com>
2015-10-30 18:11:58 -0600

    Put the reading part into function. Added an end of reccord check

--- 1.0.2/Arduino/EEPROM-Writing/EEPROM-Writing.ino
+++ 1.0.2/Arduino/EEPROM-Writing/EEPROM-Writing.ino
@@ -20,6 +20,32 @@
     return rdata;
   }
 
+void readEEPROMtoSerial()
+{
+  bool eor = false; //End Of Reccording
+  
+  int addr=0; //first address
+  int i;
+  byte b = i2c_eeprom_read_byte(0x50, 0); // access the first address from the memory
+  
+  while (eor==false) 
+  {
+    Serial.write(b); //print content to serial port
+    addr++; //increase address
+    b = i2c_eeprom_read_byte(0x50, addr); //access an address from the memory
+    
+    if(b == 255) // Check that it's not end of reccording
+    {
+      eor = true;
+      for(i=1;i<5;i++)
+      {
+        if(i2c_eeprom_read_byte(0x50, addr + i) != 255)
+        {eor = false;}
+      }
+    }
+  }
+}
+
 void setup() 
   {
     byte somedata[] = {1, 2, 3, 123, 234}; // data to write
@@ -34,27 +60,7 @@ void setup()
 
     delay(10); //add a small delay
 
-    bool eor = false;
-    
-    int addr=0; //first address
-    byte b = i2c_eeprom_read_byte(0x50, 0); // access the first address from the memory
-    
-
-    while (eor==false) 
-    {
-      Serial.write(b); //print content to serial port
-      addr++; //increase address
-      b = i2c_eeprom_read_byte(0x50, addr); //access an address from the memory
-      if(b == 255)
-      {
-        eor = true;
-        for(i=1;i<5;i++)
-        {
-          if(i2c_eeprom_read_byte(0x50, addr + i) != 255)
-          {eor = false;}
-        }
-      }
-    }
+    readEEPROMtoSerial();
   }
 
   void loop() 
