1e0b9320dcc1828a4ae4ea30c6dec796b5ea8a2f
GBhack <gbmail01@gmail.com>
2015-11-04 12:57:42 -0600

    Update

--- 1.0.2/Arduino/EEPROM-Writing/EEPROM-Writing.ino
+++ 1.0.2/Arduino/EEPROM-Writing/EEPROM-Writing.ino
@@ -1,66 +1,22 @@
  #include <Wire.h> //I2C library
-
-  void i2c_eeprom_write_byte( int deviceaddress, unsigned int eeaddress, byte data ) {
-    int rdata = data;
-    Wire.beginTransmission(deviceaddress);
-    Wire.write((int)(eeaddress >> 8)); // MSB
-    Wire.write((int)(eeaddress & 0xFF)); // LSB
-    Wire.write(rdata);
-    Wire.endTransmission();
-  }
-
-  byte i2c_eeprom_read_byte( int deviceaddress, unsigned int eeaddress ) {
-    byte rdata = 0xFF;
-    Wire.beginTransmission(deviceaddress);
-    Wire.write((int)(eeaddress >> 8)); // MSB
-    Wire.write((int)(eeaddress & 0xFF)); // LSB
-    Wire.endTransmission();
-    Wire.requestFrom(deviceaddress,1);
-    if (Wire.available()) rdata = Wire.read();
-    return rdata;
-  }
-
-void readEEPROMtoSerial()
-{
-  bool eor = false; //End Of Reccording
-  
-  int addr=0; //first address
-  int i;
-  byte b = i2c_eeprom_read_byte(0x50, 0); // access the first address from the memory
-  
-  while (eor==false) 
-  {
-    Serial.write(b); //print content to serial port
-    addr++; //increase address
-    b = i2c_eeprom_read_byte(0x50, addr); //access an address from the memory
-    
-    if(b == 255) // Check that it's not end of reccording
-    {
-      eor = true;
-      for(i=1;i<5;i++)
-      {
-        if(i2c_eeprom_read_byte(0x50, addr + i) != 255)
-        {eor = false;}
-      }
-    }
-  }
-}
-
+ #include <i2cEEPROM.h> //Our EEPROM library
+ i2cEEPROM _eeprom;
 void setup() 
   {
+
     byte somedata[] = {1, 2, 3, 123, 234}; // data to write
     Wire.begin(); // initialise the connection
     Serial.begin(9600);
     int i,j;
     for(i = 0; i<5;i++)
-    {i2c_eeprom_write_byte(0x50, i, (byte)somedata[i]); delay(5);}
+    {_eeprom.writeByte(i, (byte)somedata[i]); delay(5);}
     for(j = 0; j<6;j++)
-    {i2c_eeprom_write_byte(0x50, i+j, (byte)(255)); delay(5);}
+    {_eeprom.writeByte(i+j, (byte)(255)); delay(5);}
     
 
     delay(10); //add a small delay
 
-    readEEPROMtoSerial();
+    _eeprom.readToSerial();
   }
 
   void loop() 
--- 1.0.2/Arduino/Sensors_reading/Sensors_reading.ino
+++ 1.0.2/Arduino/Sensors_reading/Sensors_reading.ino
@@ -13,6 +13,8 @@ Adafruit_BNO055 bno = Adafruit_BNO055();
     Arduino setup function (automatically called at startup)
 */
 /**************************************************************************/
+bool CALIBRATED = false;
+
 void setup(void)
 {
   Serial.begin(9600);
@@ -27,11 +29,13 @@ void setup(void)
   }
 
   delay(1000);
-
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW); 
 
   bno.setExtCrystalUse(true);
 
-  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
+  
+  
 }
 
 /**************************************************************************/
@@ -42,43 +46,58 @@ void setup(void)
 /**************************************************************************/
 void loop(void)
 {
+  uint8_t system, gyro, accele, mag = 0;
+  
+  
+  if(CALIBRATED == false)
+  {
+    bno.getCalibration(&system, &gyro, &accele, &mag);
+      if(gyro == 3 && accele ==3)
+      {
+        CALIBRATED = true;
+        digitalWrite(13, HIGH); 
+      }
+  }
+  else
+  {
+    imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  
+    /* Display the floating point data */
+    Serial.print("X: ");
+    Serial.print(accel.x());
+    Serial.print(" Y: ");
+    Serial.print(accel.y());
+    Serial.print(" Z: ");
+    Serial.print(accel.z());
+    Serial.print("\t\t");
 
-  imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
 
-  /* Display the floating point data */
-  Serial.print("X: ");
-  Serial.print(accel.x());
-  Serial.print(" Y: ");
-  Serial.print(accel.y());
-  Serial.print(" Z: ");
-  Serial.print(accel.z());
-  Serial.print("\t\t");
-
-
-  // Quaternion data
-  imu::Quaternion quat = bno.getQuat();
-  Serial.print("qW: ");
-  Serial.print(quat.w(), 4);
-  Serial.print(" qX: ");
-  Serial.print(quat.y(), 4);
-  Serial.print(" qY: ");
-  Serial.print(quat.x(), 4);
-  Serial.print(" qZ: ");
-  Serial.print(quat.z(), 4);
-  Serial.print("\t\t");
+    // Quaternion data
+    imu::Quaternion quat = bno.getQuat();
+    Serial.print("qW: ");
+    Serial.print(quat.w(), 4);
+    Serial.print(" qX: ");
+    Serial.print(quat.y(), 4);
+    Serial.print(" qY: ");
+    Serial.print(quat.x(), 4);
+    Serial.print(" qZ: ");
+    Serial.print(quat.z(), 4);
+    Serial.print("\t\t");
+    
+  
+    /* Display calibration status for each sensor. */
+    bno.getCalibration(&system, &gyro, &accele, &mag);
+    Serial.print("CALIBRATION: Sys=");
+    Serial.print(system, DEC);
+    Serial.print(" Gyro=");
+    Serial.print(gyro, DEC);
+    Serial.print(" Accel=");
+    Serial.print(accele, DEC);
+    Serial.print(" Mag=");
+    Serial.println(mag, DEC);
+  }
   
 
-  /* Display calibration status for each sensor. */
-  uint8_t system, gyro, accele, mag = 0;
-  bno.getCalibration(&system, &gyro, &accele, &mag);
-  Serial.print("CALIBRATION: Sys=");
-  Serial.print(system, DEC);
-  Serial.print(" Gyro=");
-  Serial.print(gyro, DEC);
-  Serial.print(" Accel=");
-  Serial.print(accele, DEC);
-  Serial.print(" Mag=");
-  Serial.println(mag, DEC);
 
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
